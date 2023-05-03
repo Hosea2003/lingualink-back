551dd5a24856b10bac68763ab0dbaa646afdd273
Rindra Hosea <mahefaniainarindra@gmail.com>
2023-05-03 10:42:39 +0300

    create a custom user for lingualink

--- mod_user/models.py
+++ mod_user/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, UserManager
 from django.utils.translation import gettext_lazy as _
+from datetime import date
 
 
 class LinguaUserManager(UserManager):
@@ -15,7 +16,7 @@ class LinguaUserManager(UserManager):
             password=password,
             **extra_fields
         )
-        user.password = user.set_password(password)
+        user.set_password(password)
 
         user.save()
 
@@ -25,8 +26,13 @@ class LinguaUserManager(UserManager):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_admin", True)
+        birthdate = extra_fields.pop("birthdate", None)
+        user = self.create_user(username, email, password, **extra_fields)
+        if birthdate is None:
+            user.birthdate = date.now()
+            user.save()
 
-        return self.create_user(username, email, password, **extra_fields)
+        return user
 
 
 class LinguaUser(AbstractUser):
@@ -34,7 +40,7 @@ class LinguaUser(AbstractUser):
         MALE = 'MALE', _('MALE')
         FEMALE = 'FEMALE', _('FEMALE')
 
-    birthdate = models.DateField()
+    birthdate = models.DateField(null=True)
     gender = models.CharField(
         max_length=20,
         choices=GenderChoice.choices,
@@ -46,3 +52,8 @@ class LinguaUser(AbstractUser):
     class Meta:
         db_table = 'LINGUA_USER'
         db_table_comment = 'user used for lingualink app'
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.set_password(self.password)
+        super().save(*args, **kwargs)
